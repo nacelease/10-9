ce57c0452352cfe88c32b4fa533e05bdc22913a0
nacelease <54339008+nacelease@users.noreply.github.com>
2019-10-09 19:48:56 -0500

    Add files via upload

---
+++ dinosaurs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _1.Data
+{
+    public class dinosaurs
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DinoID { get; set; }
+        [Display(Name ="Dinosaur name")]
+        public string DName { get; set; }
+        [Display(Name ="Exhibit Number")]
+        public int Enumber{ get; set; }
+
+        [ForeignKey("Enumber")]
+        public exhibits Etypes { get; set; }
+    }
+}
